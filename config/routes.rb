597c58d4a098454f4a6cfe76b4db4e03a7d8bc64
Laurene Freyer <laurene.lepvrier@hotmail.fr>
2023-06-19 17:04:21 +0200

    start controller reservations

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :pets
-    # resources :reservations, only: [:create, :edit, :update]
-
+  resources :pets do
+    resources :reservations, only: [:create, :edit, :update, :index]
+  end
 end
