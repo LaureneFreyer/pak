fa9aa5deb13d275d20f3a2aa09bf907a5c2b1beb
Laurene Freyer <laurene.lepvrier@hotmail.fr>
2023-06-19 14:52:19 +0200

    add models

---
+++ db/migrate/20230619124238_create_pets.rb
@@ -0,0 +1,15 @@
+class CreatePets < ActiveRecord::Migration[7.0]
+  def change
+    create_table :pets do |t|
+      t.string :name
+      t.string :address
+      t.integer :price
+      t.string :content
+      t.integer :rating
+      t.string :photo
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
