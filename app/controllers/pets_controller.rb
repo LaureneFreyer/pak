18ab96ed81d233a860b44cd7177b920c5aee11ec
Cyrille Varin <cyrille.varin@live.fr>
2023-06-20 01:29:00 +0200

    pas mal de changement connection user

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,5 +1,6 @@
 class PetsController < ApplicationController
   before_action :find_pet, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: :index
 
   def index
     @pets = Pet.all
