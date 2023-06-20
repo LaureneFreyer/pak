b1760b959766eef6e17a6d17cdc10b05cc1231d3
Laurene Freyer <laurene.lepvrier@hotmail.fr>
2023-06-20 10:37:42 +0200

    pb db

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,9 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-ActiveRecord::Schema[7.0].define(version: 2023_06_19_155223) do
+
+
+ActiveRecord::Schema[7.0].define(version: 2023_06_19_213612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
