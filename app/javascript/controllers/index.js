9cd6e1d1ba0b9a07f381494e40e5ddd9c94389c5
Laurene Freyer <laurene.lepvrier@hotmail.fr>
2023-06-22 17:57:32 +0200

    modifs

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import AddressAutocompleteController from "./address_autocomplete_controller"
+application.register("address-autocomplete", AddressAutocompleteController)
+
 import AnimtextController from "./animtext_controller"
 application.register("animtext", AnimtextController)
 
