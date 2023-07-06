418d4c17d1002c056a424109db9b6b723b7b1c2e
Cyrille Varin <cyrille.varin@live.fr>
2023-07-06 22:26:57 +0200

    finish

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,7 +1,7 @@
 class PetsController < ApplicationController
   before_action :find_pet, only: [:show, :edit, :update, :destroy]
   before_action :map, only: [:index]
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     if params[:query].present?
